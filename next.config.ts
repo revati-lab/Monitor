cf483e7eadc4c849db393c5ba6933689101bee07
Revathi <Revatikui6@gmail.com>
2026-01-22 20:28:50 -0500

    Fixed the next js config

--- next.config.ts
+++ next.config.ts
@@ -1,11 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false,
-    buildActivityPosition: "bottom-right",
-  },
+  devIndicators: false,
 };
 
 export default nextConfig;
